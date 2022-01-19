7ad0c617db4deb43917406a9b9f0ce71af47d261
zhangxianlei <zhangxianlei@uphicoo.com>
2022-01-19 11:04:00 +0800

    docs: ✏️ 添加文档注释

--- src/hooks/use-count/index.ts
+++ src/hooks/use-count/index.ts
@@ -1,17 +1,21 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 
+/** ## 计数
+ * 可用于短信计时,秒表等场合
+ * - defaultCount - 开始计数的初始值
+ */
 const useCount = (
   defaultCount: number,
-  options?: {
+  options: {
     /** 计数步频,默认为 1 */
     step?: number;
     /** 计数间隔 单位为ms,默认 1000 */
     during?: number;
     /** 结束时的值,默认为0 */
     end?: number;
-  }
+  } = {}
 ) => {
-  const { step = 1, during = 1000, end = 0 } = options || {};
+  const { step = 1, during = 1000, end = 0 } = options;
 
   const Timer = useRef<NodeJS.Timeout>();
 
