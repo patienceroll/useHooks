45cab7cc74b9cc7128f8e6bc377dc471a17cac65
zhangxianlei <zhangxianlei@uphicoo.com>
2022-10-19 15:11:02 +0800

    feat: 🎸 实现文件分片demo

--- src/demo/use-splice-file/index.tsx
+++ src/demo/use-splice-file/index.tsx
@@ -1,20 +1,44 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
+
+function* createContext(file: File, sliceSize: number) {
+  let index = 1;
+  const fileSize = file.size;
+  const length = Math.ceil(fileSize / sliceSize);
+  while (index <= length) {
+    yield (function ($index: number) {
+      return () =>
+        file.slice(
+          ($index - 1) * sliceSize,
+          $index === length ? file.size : $index * sliceSize
+        );
+    })(index);
+    index += 1;
+  }
+}
 
 export default function useSpliceFile() {
-  const Buffer = useRef(new ArrayBuffer(1024 * 1024));
-  const View = useRef(new DataView(Buffer.current))
+  const [slicedArray, setSlicedArray] = useState<(() => Blob)[]>([]);
   return (
     <div>
       <input
         type="file"
         onChange={(event) => {
-          if (event.target.files) {
-            const file = event.target.files.item(0) as File
-            file.stream().getReader().read()
-            
+          if (event.target.files && event.target.files.item(0)) {
+            const file = event.target.files.item(0) as File;
+            setSlicedArray(Array.from(createContext(file, 1024 * 1024)));
           }
         }}
       />
+      <h2>分片结果</h2>
+      {slicedArray.map((item, index) => {
+        const blob = item();
+        console.log(blob);
+        return (
+          <div key={index + Math.random()}>
+            {`${index + 1}: { size: ${blob.size}, type: ${blob.type} }`}
+          </div>
+        );
+      })}
     </div>
   );
 }
